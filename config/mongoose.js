d47383877d22ee2e1a53f9a0a6fff194eb3cdd2c
Nicas Heydorn <nicas.kilian.heydorn@commerce-plus.com>
2015-06-14 14:22:48 +0200

    Form

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,4 @@
-require('mongoose').connect(process.env.DB_URL || 'mongodb://localhost:27017/films');
+require('mongoose').connect(process.env.DB_URL || 'mongodb://localhost:27017/cheesr');
 
 require(require('path').join(__dirname, '..', 'server', 'models', 'filmModel'));
 require(require('path').join(__dirname, '..', 'server', 'models', 'userModel'));
