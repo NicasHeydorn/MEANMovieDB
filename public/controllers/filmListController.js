f7cfa50fbc2134acb2e65ec52aa7f8035d3d6357
Nicas Heydorn <nicas.kilian.heydorn@commerce-plus.com>
2015-06-14 11:34:09 +0200

    Sorting and Merge

--- public/controllers/filmListController.js
+++ public/controllers/filmListController.js
@@ -3,6 +3,14 @@ angular.module('nak-node-seminar').controller('FilmListController', ['$scope', '
 
     Film.query().$promise.then(function (films) {
         $scope.films = films;
+        /*.sort(function(a, b, key) {
+        if ($scope.hasOwnProperty('sortBy')) {
+            key = $scope.sortBy;
+        } else {
+            key = 'title';
+        };
+        return (a[key] > b[key]) - (a[key] < b[key]);
+    });*/
     });
 
     Genre.query().$promise.then(function (genres) {
